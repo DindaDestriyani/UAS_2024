eedc218ebc64e7fc05640eb8b0a3ef1e726d1f01
DindaDestriyaniHernawati <dindadestriyani939@gmail.com>
2024-01-15 16:32:08 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,37 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class PendeteksiKecepatan {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> Kecepatan1 = new ArrayList<>();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.print("Masukan Angka = ");
+        Scanner inputUser = new Scanner(System.in);
+        int number = inputUser.nextInt();
+        int batas_atas = 200;
+        int batas_bawah = 0;
+        while (number <= batas_atas) {
+            System.out.println("Angka = " + number);
+            number++;
+
+            for (int i = 0; i < 200; i++) {
+                int kecepatan = Kecepatan1.get(0);
+                Kecepatan1.add(kecepatan);
+            }
+
+            System.out.println("Kecepatan antara 80 km/jam dan 100 km/jam");
+            for (int kecepatan : Kecepatan1) {
+                if (kecepatan >= 80 && kecepatan <= 100) {
+                    System.out.println(kecepatan + " km/jam ");
+                }
+            }
+            System.out.println("Kecepatan di bawah 80 km/jam");
+            for (int kecepatan : Kecepatan1) {
+                if (kecepatan < 80) {
+                    System.out.println(kecepatan + " km/jam ");
+                }
+            }
         }
     }
 }
